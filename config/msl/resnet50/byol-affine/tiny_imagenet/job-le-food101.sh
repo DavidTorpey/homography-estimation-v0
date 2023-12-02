94fa7f2cb05c8a894ef47c80b8b8c125a485bb4f
DavidTorpey <torpey.david93@gmail.com>
2023-12-02 15:07:10 +0000

    v1.0

--- config/msl/resnet50/byol-affine/tiny_imagenet/job-le-food101.sh
+++ config/msl/resnet50/byol-affine/tiny_imagenet/job-le-food101.sh
@@ -10,6 +10,6 @@ cd /home-mscluster/dtorpey/code/homography-estimation-v0
 
 . /home-mscluster/dtorpey/code/object-centricity-ssl/env.sh
 
-python -m he.linear_eval.run \
+python -m he.linear_eval.le \
 --config_path /home-mscluster/dtorpey/code/homography-estimation-v0/config/msl/resnet50/byol-affine/tiny_imagenet/config-le-food101.yaml \
 --model_path /home-mscluster/dtorpey/code/homography-estimation-v0/results/resnet50/byol-affine/tiny_imagenet/90_model_tiny_imagenet.pth
--- config/msl/resnet50/simclr/tiny_imagenet/job-le-cifar100.sh
+++ config/msl/resnet50/simclr/tiny_imagenet/job-le-cifar100.sh
@@ -10,6 +10,6 @@ cd /home-mscluster/dtorpey/code/homography-estimation-v0
 
 . /home-mscluster/dtorpey/code/object-centricity-ssl/env.sh
 
-python -m he.linear_eval.run \
+python -m he.linear_eval.le \
 --config_path /home-mscluster/dtorpey/code/homography-estimation-v0/config/msl/resnet50/simclr/tiny_imagenet/config-le-cifar100.yaml \
 --model_path /home-mscluster/dtorpey/code/homography-estimation-v0/results/resnet50/simclr/tiny_imagenet/90_model_tiny_imagenet.pth
