1cb805da4b938875e727637fba6ca7b762aaa037
DavidTorpey <torpey.david93@gmail.com>
2023-12-03 13:33:30 +0000

    v1.0

--- he/trainer/barlow_twins.py
+++ he/trainer/barlow_twins.py
@@ -10,7 +10,6 @@ from lightly.loss import BarlowTwinsLoss
 from torch import nn
 
 from he.configuration import Config
-from he.nt_xent import NTXentLoss
 
 
 class BarlowTwinsTrainer:
@@ -209,6 +208,141 @@ class BarlowTwinsAffineTrainer:
                 gt_params = gt_params.to(self.device)
 
                 loss = self._step(xis, xjs, xits, gt_params)
+
+                loss.backward()
+
+                train_loss += float(loss.item())
+
+                self.optimizer.step()
+                n_iter += 1
+            train_loss /= len(train_loader)
+
+            valid_loss = self._validate(val_loader)
+
+            logging.info({'train/loss': train_loss, 'val/loss': valid_loss})
+            if self.config.general.log_to_wandb:
+                wandb.log({'train/loss': train_loss, 'val/loss': valid_loss})
+
+            if valid_loss < best_valid_loss:
+                best_valid_loss = valid_loss
+                torch.save(
+                    self.model.state_dict(),
+                    os.path.join(self.run_folder, self.model_name)
+                )
+
+            if epoch_counter >= self.warmup_steps:
+                self.scheduler.step()
+
+            valid_n_iter += 1
+
+            if epoch_counter % 10 == 0:
+                torch.save(
+                    self.model.state_dict(),
+                    os.path.join(self.run_folder, str(epoch_counter) + '_' + self.model_name)
+                )
+
+
+
+class BarlowTwinsDoubleAffineTrainer:
+    def __init__(self, model, param_head, optimizer, scheduler, config: Config):
+        self.model = model
+        self.param_head = param_head
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+        self.batch_size = config.trainer.batch_size
+        self.epochs = config.trainer.epochs
+        self.device = config.trainer.device
+        self.dataset = config.data.dataset
+        self.run_folder = config.general.output_dir
+        self.warmup_steps = config.trainer.warmup_epochs
+        self.config = config
+
+        self.criterion = BarlowTwinsLoss()
+
+        self.mse_criterion = nn.MSELoss()
+
+        self.model_name = 'model_{}.pth'.format(self.dataset)
+
+        if config.general.log_to_wandb:
+            wandb.init(
+                project='phd', config=asdict(config),
+                name=f'BarlowTwins Affine : {config.data.dataset}',
+                tags=[f'run_id: {config.general.run_id}'],
+            )
+
+    def compute_affine_loss(self, xts, rs, gt_params):
+        rts, _ = self.model(xts)
+        if self.config.network.aggregation_strategy == 'diff':
+            transition_vector = rs - rts
+        elif self.config.network.aggregation_strategy == 'concat':
+            transition_vector = torch.cat([rs, rts], dim=-1)
+        else:
+            raise Exception(f'Invalid aggregation strategy: {self.config.network.aggregation_strategy}')
+        params_dist = self.param_head(transition_vector)
+        param_loss = self.mse_criterion(params_dist, gt_params)
+        return param_loss
+
+    def _step(self, xis, xjs, xits, gti_params, xjts, gtj_params):
+        ris, zis = self.model(xis)  # [N,C]
+
+        # get the representations and the projections
+        rjs, zjs = self.model(xjs)  # [N,C]
+
+        # normalize projection feature vectors
+        zis = F.normalize(zis, dim=1)
+        zjs = F.normalize(zjs, dim=1)
+
+        loss = self.criterion(zis, zjs)
+
+        param_loss_i = self.compute_affine_loss(xits, ris, gti_params)
+        param_loss_j = self.compute_affine_loss(xjts, rjs, gtj_params)
+
+        return loss + param_loss_i + param_loss_j
+
+    def _validate(self, val_loader):
+        with torch.no_grad():
+            self.model.eval()
+            self.param_head.eval()
+
+            valid_loss = 0.0
+            counter = 0
+            for xis, xjs, xits, gti_params, xjts, gtj_params in val_loader:
+                xis = xis.to(self.device)
+                xjs = xjs.to(self.device)
+                xits = xits.to(self.device)
+                gti_params = gti_params.to(self.device)
+                xjts = xjts.to(self.device)
+                gtj_params = gtj_params.to(self.device)
+
+                loss = self._step(xis, xjs, xits, gti_params, xjts, gtj_params)
+                valid_loss += loss.item()
+                counter += 1
+            valid_loss /= counter
+        self.model.train()
+        self.param_head.train()
+
+        return valid_loss
+
+    def train(self, train_loader, val_loader):
+        n_iter = 0
+        valid_n_iter = 0
+        best_valid_loss = np.inf
+
+        for epoch_counter in range(self.epochs):
+            logging.info('%s/%s', epoch_counter + 1, self.epochs)
+
+            train_loss = 0.0
+            for xis, xjs, xits, gti_params, xjts, gtj_params in train_loader:
+                self.optimizer.zero_grad()
+
+                xis = xis.to(self.device)
+                xjs = xjs.to(self.device)
+                xits = xits.to(self.device)
+                gti_params = gti_params.to(self.device)
+                xjts = xjts.to(self.device)
+                gtj_params = gtj_params.to(self.device)
+
+                loss = self._step(xis, xjs, xits, gti_params, xjts, gtj_params)
 
                 loss.backward()
 
--- he/trainer/byol.py
+++ he/trainer/byol.py
@@ -240,3 +240,141 @@ class BYOLAffineTrainer:
                     self.model.state_dict(),
                     os.path.join(self.run_folder, str(epoch_counter) + '_' + self.model_name)
                 )
+
+
+
+class BYOLDoubleAffineTrainer:
+    def __init__(self, model: BYOL, param_head, optimizer, scheduler, config: Config):
+        self.model = model
+        self.param_head = param_head
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+        self.batch_size = config.trainer.batch_size
+        self.epochs = config.trainer.epochs
+        self.device = config.trainer.device
+        self.dataset = config.data.dataset
+        self.run_folder = config.general.output_dir
+        self.warmup_steps = config.trainer.warmup_epochs
+        self.config = config
+
+        self.m_base = 0.996
+        self.m = 0.996
+
+        self.mse_criterion = nn.MSELoss()
+
+        self.model_name = 'model_{}.pth'.format(self.dataset)
+
+    @staticmethod
+    def regression_loss(x, y):
+        x = F.normalize(x, dim=1)
+        y = F.normalize(y, dim=1)
+        return 2 - 2 * (x * y).sum(dim=-1)
+
+    def compute_affine_loss(self, xts, rs, gt_params):
+        rts, _ = self.model(xts)
+        if self.config.network.aggregation_strategy == 'diff':
+            transition_vector = rs - rts
+        elif self.config.network.aggregation_strategy == 'concat':
+            transition_vector = torch.cat([rs, rts], dim=-1)
+        else:
+            raise Exception(f'Invalid aggregation strategy: {self.config.network.aggregation_strategy}')
+        params_dist = self.param_head(transition_vector)
+        param_loss = self.mse_criterion(params_dist, gt_params)
+        return param_loss
+
+    def _step(self, xis, xjs, xits, gti_params, xjts, gtj_params):
+        ris1, zis1 = self.model.online_network(xis)
+        predictions_from_view_1 = self.model.predictor(zis1)
+
+        ris2, zis2 = self.model.online_network(xjs)
+        predictions_from_view_2 = self.model.predictor(zis2)
+
+        with torch.no_grad():
+            _, targets_to_view_2 = self.model.target_network(xis)
+            _, targets_to_view_1 = self.model.target_network(xjs)
+
+        loss = self.regression_loss(predictions_from_view_1, targets_to_view_1)
+        loss += self.regression_loss(predictions_from_view_2, targets_to_view_2)
+
+        param_loss_i = self.compute_affine_loss(xits, ris1, gti_params)
+        param_loss_j = self.compute_affine_loss(xjts, ris2, gtj_params)
+
+        return loss.mean() + param_loss_i + param_loss_j
+
+    def _validate(self, val_loader):
+        with torch.no_grad():
+            self.model.eval()
+            self.param_head.eval()
+
+            valid_loss = 0.0
+            counter = 0
+            for xis, xjs, xits, gti_params, xjts, gtj_params in val_loader:
+                xis = xis.to(self.device)
+                xjs = xjs.to(self.device)
+                xits = xits.to(self.device)
+                gti_params = gti_params.to(self.device)
+                xjts = xjts.to(self.device)
+                gtj_params = gtj_params.to(self.device)
+
+                loss = self._step(xis, xjs, xits, gti_params, xjts, gtj_params)
+                valid_loss += loss.item()
+                counter += 1
+            valid_loss /= counter
+        self.model.train()
+        self.param_head.train()
+
+        return valid_loss
+
+    @torch.no_grad()
+    def _update_target_network_parameters(self):
+        for param_q, param_k in zip(self.model.online_network.parameters(), self.model.target_network.parameters()):
+            param_k.data = param_k.data * self.m + param_q.data * (1. - self.m)
+
+    def train(self, train_loader, val_loader):
+        K = len(train_loader) * self.epochs
+
+        n_iter = 0
+        valid_n_iter = 0
+        best_valid_loss = np.inf
+
+        for epoch_counter in range(self.epochs):
+            logging.info('%s/%s', epoch_counter + 1, self.epochs)
+            for xis, xjs, xits, gti_params, xjts, gtj_params in train_loader:
+                self.optimizer.zero_grad()
+
+                xis = xis.to(self.device)
+                xjs = xjs.to(self.device)
+                xits = xits.to(self.device)
+                gti_params = gti_params.to(self.device)
+                xjts = xjts.to(self.device)
+                gtj_params = gtj_params.to(self.device)
+
+                loss = self._step(xis, xjs, xits, gti_params, xjts, gtj_params)
+
+                loss.backward()
+
+                self.optimizer.step()
+                n_iter += 1
+
+                self._update_target_network_parameters()
+
+                self.m = 1 - (1 - self.m_base) * (np.cos(np.pi * n_iter / K) + 1) / 2
+
+            valid_loss = self._validate(val_loader)
+            if valid_loss < best_valid_loss:
+                best_valid_loss = valid_loss
+                torch.save(
+                    self.model.state_dict(),
+                    os.path.join(self.run_folder, self.model_name)
+                )
+
+            if epoch_counter >= self.warmup_steps:
+                self.scheduler.step()
+
+            valid_n_iter += 1
+
+            if epoch_counter % 10 == 0:
+                torch.save(
+                    self.model.state_dict(),
+                    os.path.join(self.run_folder, str(epoch_counter) + '_' + self.model_name)
+                )
