f37c7471d1ef21029770227cae52970969209416
DavidTorpey <torpey.david93@gmail.com>
2023-12-03 00:57:55 +0000

    v1.0

--- config/msl/resnet50/byol-affine/tiny_imagenet/affine-components-ablations/scale-shear/job-le-cifar10.sh
+++ config/msl/resnet50/byol-affine/tiny_imagenet/affine-components-ablations/scale-shear/job-le-cifar10.sh
@@ -10,6 +10,6 @@ cd /home-mscluster/dtorpey/code/homography-estimation-v0
 
 . /home-mscluster/dtorpey/code/object-centricity-ssl/env.sh
 
-python -m he.linear_eval.run \
+python -m he.linear_eval.le \
 --config_path /home-mscluster/dtorpey/code/homography-estimation-v0/config/msl/resnet50/byol-affine/tiny_imagenet/affine-components-ablations/scale-shear/config-le-cifar10.yaml \
 --model_path /home-mscluster/dtorpey/code/homography-estimation-v0/results/resnet50/byol-affine/tiny_imagenet/affine-components-ablations/scale-shear/90_model_tiny_imagenet.pth
